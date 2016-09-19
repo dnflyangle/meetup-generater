ccaeebe95b6557c3dd82cb1f6d7a46ea80b32a04
Isabella Li <dnflyangle@gmail.com>
2016-09-19 11:50:24 +1000

    add iot and all api meetups

--- lib/meetupinator/event_finder.rb
+++ lib/meetupinator/event_finder.rb
@@ -1,10 +1,31 @@
 module Meetupinator
   # class def
   class EventFinder
+    DAY = 60 * 60 * 24
+
     def extract_events(group_url_names, api, weeks)
       ids = group_url_names.map { |name| api.get_meetup_id name }
 
       api.get_upcoming_events(ids, weeks)
+    end
+
+    def filter_internal_events(events)
+      events.keep_if { |event| }
+
+    end
+
+    private
+
+    def get_start_of_week
+      d = Time.now
+      d -= DAY until d.monday?
+      d
+    end
+
+    def get_end_of_week
+      d = Time.now
+      d += DAY until d.sunday?
+      d
     end
   end
 end
